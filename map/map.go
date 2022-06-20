e2542d349fe4e914aabaa977034b1104de971b39
xinyubot <xinyubot@163.com>
2022-06-20 14:47:06 +0800

    ignore: naming tweak

--- map/map.go
+++ map/map.go
@@ -6,8 +6,8 @@ import (
 	"sync"
 )
 
-// mMap embeds a lock and a generic map[K]V.
-type mMap[K comparable, V any] struct {
+// mmap embeds a lock and a generic map[K]V.
+type mmap[K comparable, V any] struct {
 	lock *sync.Mutex // to keep the Mutex-related fields and methods from being exported
 	m    map[K]V     // underlying map that actually stores the K-V pairs
 }
@@ -20,8 +20,8 @@ type keyval[K comparable, V any] struct {
 
 // NewMap returns a pointer to a newly instantiated Map,
 // with the underlying map allocated with enough space to hold the specified number of elements.
-func NewMap[K comparable, V any](size ...uint64) *mMap[K, V] {
-	ret := &mMap[K, V]{lock: new(sync.Mutex)}
+func NewMap[K comparable, V any](size ...uint64) *mmap[K, V] {
+	ret := &mmap[K, V]{lock: new(sync.Mutex)}
 	switch len(size) {
 	case 0:
 		ret.m = make(map[K]V)
@@ -31,18 +31,18 @@ func NewMap[K comparable, V any](size ...uint64) *mMap[K, V] {
 	return ret
 }
 
-// Set sets a K-V pair into the mMap, and returns the pointer to the mMap.
-func (mm *mMap[K, V]) Set(key K, value V) *mMap[K, V] {
+// Set sets a K-V pair into the mmap, and returns the pointer to the mmap.
+func (mm *mmap[K, V]) Set(key K, value V) *mmap[K, V] {
 	mm.lock.Lock()
 	mm.m[key] = value
 	mm.lock.Unlock()
 	return mm
 }
 
-// Get returns the value V from the mMap given a key K and a bool indicating whether the key K exists.
+// Get returns the value V from the mmap given a key K and a bool indicating whether the key K exists.
 // If the key does exists, Get returns the value V, and true.
 // Otherwise Get returns the specified default value defval, and false.
-func (mm *mMap[K, V]) Get(key K, defval V) (V, bool) {
+func (mm *mmap[K, V]) Get(key K, defval V) (V, bool) {
 	mm.lock.Lock()
 	ret, ok := mm.m[key]
 	mm.lock.Unlock()
@@ -52,26 +52,26 @@ func (mm *mMap[K, V]) Get(key K, defval V) (V, bool) {
 	return ret, ok
 }
 
-// Del removes a K-V pair from the mMap.
+// Del removes a K-V pair from the mmap.
 // If there is no such key K, Del is a no-op.
-func (mm *mMap[K, V]) Del(key K) *mMap[K, V] {
+func (mm *mmap[K, V]) Del(key K) *mmap[K, V] {
 	mm.lock.Lock()
 	delete(mm.m, key)
 	mm.lock.Unlock()
 	return mm
 }
 
-// Len returns the number of K-V pairs in the mMap.
-func (mm *mMap[K, V]) Len() int {
+// Len returns the number of K-V pairs in the mmap.
+func (mm *mmap[K, V]) Len() int {
 	mm.lock.Lock()
 	ret := len(mm.m)
 	mm.lock.Unlock()
 	return ret
 }
 
-// JsonFormatter prints the mMap in a pretty JSON format with indent.
+// JsonFormatter prints the mmap in a pretty JSON format with indent.
 // For debug purposes only.
-func (mm *mMap[K, V]) JsonFormatter() {
+func (mm *mmap[K, V]) JsonFormatter() {
 	mm.lock.Lock()
 	s, _ := json.MarshalIndent(mm.m, "", "  ")
 	mm.lock.Unlock()
@@ -79,7 +79,7 @@ func (mm *mMap[K, V]) JsonFormatter() {
 }
 
 // MapKeys returns the slice of all Keys in the map.
-func (mm *mMap[K, V]) Keys() []K {
+func (mm *mmap[K, V]) Keys() []K {
 	ret := make([]K, 0, len(mm.m))
 	for k := range mm.m {
 		ret = append(ret, k)
@@ -89,7 +89,7 @@ func (mm *mMap[K, V]) Keys() []K {
 }
 
 // MapValues returns the slice of all Values in the map.
-func (mm *mMap[K, V]) Values(V) []V {
+func (mm *mmap[K, V]) Values(V) []V {
 	mm.lock.Lock()
 	ret := make([]V, 0, len(mm.m))
 	for _, v := range mm.m {
@@ -99,9 +99,9 @@ func (mm *mMap[K, V]) Values(V) []V {
 	return ret
 }
 
-// Filter filters out the K-V pairs from the mMap by the specified filtering fuction f, and returns the original mMap.
-// f should return false if the K-V pair is desired to be removed from the mMap.
-func (mm *mMap[K, V]) Filter(f func(key K, value V) bool) *mMap[K, V] {
+// Filter filters out the K-V pairs from the mmap by the specified filtering fuction f, and returns the original mmap.
+// f should return false if the K-V pair is desired to be removed from the mmap.
+func (mm *mmap[K, V]) Filter(f func(key K, value V) bool) *mmap[K, V] {
 	mm.lock.Lock()
 	for k, v := range mm.m {
 		if !f(k, v) {
@@ -112,9 +112,9 @@ func (mm *mMap[K, V]) Filter(f func(key K, value V) bool) *mMap[K, V] {
 	return mm
 }
 
-// FilterCopy returns a filtered mMap copied from the original mMap.
-// f should return false if the K-V pair is desired to be removed from the mMap.
-func (mm *mMap[K, V]) FilterCopy(f func(key K, value V) bool) *mMap[K, V] {
+// FilterCopy returns a filtered mmap copied from the original mmap.
+// f should return false if the K-V pair is desired to be removed from the mmap.
+func (mm *mmap[K, V]) FilterCopy(f func(key K, value V) bool) *mmap[K, V] {
 	mm.lock.Lock()
 	ret := NewMap[K, V](uint64(len(mm.m)))
 	for k, v := range mm.m {
@@ -126,9 +126,9 @@ func (mm *mMap[K, V]) FilterCopy(f func(key K, value V) bool) *mMap[K, V] {
 	return ret
 }
 
-// ToSlice returns a slice of K-V pairs in the mMap.
+// ToSlice returns a slice of K-V pairs in the mmap.
 // Notice that the order of the K-V pairs is not guaranteed.
-func (mm *mMap[K, V]) ToSlice() []keyval[K, V] {
+func (mm *mmap[K, V]) ToSlice() []keyval[K, V] {
 	mm.lock.Lock()
 	ret := make([]keyval[K, V], 0, len(mm.m))
 	for k, v := range mm.m {
